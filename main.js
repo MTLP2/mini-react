79d0cee1f3c3b12e35252f377e4df6bcd8f201ab
Mathéo Lopes <matheo.lps91@gmail.com>
2026-04-28 22:52:20 +0200

    git init

---
+++ main.js
@@ -0,0 +1,57 @@
+const element = {
+  type: "h1",
+  props: {
+    title: foo,
+    children: "Hello",
+  },
+};
+
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
+
+function createElement(type, props, children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child),
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Reacto = {
+  createElement,
+};
+
+const gigaelement = Reacto.createElement(
+  "div",
+  { id: "super" },
+  Reacto.createElement("a", null, "test"),
+  Reacto.createElement("p", null, "32"),
+);
+
+/** @jsx Reacto.createElement */
+const gigaelementtojsx = (
+  <div>
+    <a>test</a>
+    <p>52</p>
+  </div>
+);
